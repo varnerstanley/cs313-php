d71de4bf3a4746c8301789c85f248b653795751a
varnerstanley <stanmanjr28@gmail.com>
2019-01-20 16:39:11 -0700

    link

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@
     <header>
       <div class="topnav" id="myTopnav">
         <a href="#home" class="active">Home</a>
-        <a href="#news">News</a>
+        <a href="assignments.php">Assignments</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
