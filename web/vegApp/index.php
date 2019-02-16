4666606a8a22d5b806c7d76587c7cb7c1ca57b6e
Stanley Varner <stanmanjr28@gmail.com>
2019-02-15 22:54:00 -0700

    styles

--- web/vegApp/index.php
+++ web/vegApp/index.php
@@ -11,7 +11,7 @@
     <header>
       <div class="topnav" id="myTopnav">
         <a href="#home" class="active">Home</a>
-        <a href="assignments.php">Veggie Builder</a>
+        <a href="vbuilder.php">Veggie Builder</a>
         <a href="#about">About</a>
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
           <i class="fa fa-bars"></i>
---
+++ web/vegApp/vbuilder.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <title>Home Page</title>
+  </head>
+  <body>
+    <header>
+      <div class="topnav" id="myTopnav">
+        <a href="#home" class="active">Home</a>
+        <a href="vbuilder.php">Veggie Builder</a>
+        <a href="#about">About</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
+      </div>
+    </header>
+
+    <main>
+      <h1>Coming Soon...</h1>
+      <h2>Figuring out the logic now...</h2>
+
+    </main>
+
+    <footer>
+        Date <?php echo date('j F, Y', getlastmod()); ?>
+    </footer>
+    <script type="text/javascript" src="js/nav.js"></script>
+  </body>
+</html>
