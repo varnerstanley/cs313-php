e76b8db254f9c510253f448014c15ed05d533dfb
Stanley Varner <stanmanjr28@gmail.com>
2019-02-24 17:06:13 -0700

    yesss

--- web/php/vbuilder.php
+++ web/php/vbuilder.php
@@ -1,5 +1,22 @@
 <?php
 require_once 'connectdb.php';
+    // $statement = $db->prepare('INSERT INTO names(vegetable_name) VALUES(:vegetable_name)');
+    $statement->execute([
+      ':vegetable_name' => $_POST['vegetable_name']]);
+    $vegetable_id = $db->lastInsertId();
+
+    $sql = 'INSERT INTO names(vegetable_name) VALUES(:vegetable_name)';
+    // Create the prepared statement using the acme connection
+    $stmt = $db->prepare($sql);
+    // The next four lines replace the placeholders in the SQL
+    // statement with the actual values in the variables
+    // and tells the database the type of data it is
+    $stmt->bindValue(':vegetable_name', $vegetable_name, PDO::PARAM_STR);
+     $stmt->execute();
+    // Close the database interaction
+    $stmt->closeCursor();
+
+
    if( $_POST["vegetable_name"] ) {
       // if (preg_match("/[^A-Za-z'-]/",$_POST['vegetable_name'] )) {
       //    die ("invalid name and name should be alpha");
