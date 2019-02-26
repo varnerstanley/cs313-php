b37bbe4cb6ae90956cdef65db2604ba09e0b47a4
Stanley Varner <stanmanjr28@gmail.com>
2019-02-26 07:47:56 -0700

    yes

--- web/php/vbuilder.php
+++ web/php/vbuilder.php
@@ -1,12 +1,12 @@
 <?php
 require_once 'connectdb.php';
-   if( $_POST["vegetable_name"] ) {
-      if (preg_match("/[^A-Za-z'-]/",$_POST['vegetable_name'] )) {
-         die ("invalid name and name should be alpha");
-      }
-      echo "Vegetable name you added ". $_POST['vegetable_name']. "<br />";
-      exit();
-   }
+  //  if( $_POST["vegetable_name"] ) {
+  //     if (preg_match("/[^A-Za-z'-]/",$_POST['vegetable_name'] )) {
+  //        die ("invalid name and name should be alpha");
+  //     }
+  //     echo "Vegetable name you added ". $_POST['vegetable_name']. "<br />";
+  //     exit();
+  //  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +14,7 @@ require_once 'connectdb.php';
 
 
   <div class="dropdown">
-    <form action="vbuilder.php" method="POST">
+    <form action="createNew.php" method="POST">
        Add Vegetable: <input type = "text" name = "vegetable_name" />
        <input type = "submit" />
     </form>
