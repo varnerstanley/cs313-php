eaa901ae74096013b725804facb558323a50c692
Stanley Varner <stanmanjr28@gmail.com>
2019-03-01 13:08:29 -0700

    from new comp

---
+++ web/php/updateVeggie.php
@@ -0,0 +1,36 @@
+<?php
+
+$vegetable_name = $_PUT['description'];
+
+require_once 'connectdb.php';
+
+
+
+try
+{
+// $db = vegConnect();
+$query = 'UPDATE names SET description = :description WHERE veg_id = :veg_id';
+$statement = $db->prepare($query);
+
+$statement->bindValue(':description', $description);
+
+$statement->execute();
+// $vegId = $db->lastInsertId("veg_id_seq");
+
+
+
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
+// finally, redirect them to a new page to actually show the topics
+header("Location: index.php");
+die(); // we always include a die after redirects. In this case, there would be no
+       // harm if the user got the rest of the page, because there is nothing else
+       // but in general, there could be things after here that we don't want them
+       // to see.
+?>
---
+++ web/php/viewupdate.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'connectdb.php';
+  //  if( $_POST["vegetable_name"] ) {
+  //     if (preg_match("/[^A-Za-z'-]/",$_POST['vegetable_name'] )) {
+  //        die ("invalid name and name should be alpha");
+  //     }
+  //     echo "Vegetable name you added ". $_POST['vegetable_name']. "<br />";
+  //     exit();
+  //  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include("page/head.php"); ?>
+
+<div class="dropdown">
+  <form action="updateVeggie.php" method="POST">
+     Select Vegetable to Update:
+     <?php
+       echo '<select name="vegetables">'; // Open your drop down box
+       // Loop through the query results, outputing the options one by one
+       foreach ($db->query('SELECT vegetable_name, description FROM names') as $row) {
+          echo '<option value="'.$row['vegetable_name'].'" selected>'.$row['vegetable_name'].'</option>';
+       }
+       echo '</select>';// Close your drop down box
+       echo '</div>';
+     ?>
+     <input type="submit">
+  </form>
+</div>
+
+
+<?php include("page/footer.php"); ?>
+</body>
+</html>
